7b745a290afa4d38d8abd5fb2844aeba47dcaa1e
Yuki Fujiwara <fuzi1130sw@gmail.com>
2025-05-14 15:20:52 +0900

    small change

--- install.sh
+++ install.sh
@@ -29,4 +29,9 @@ curl -fsSL "https://raw.githubusercontent.com/fuji029/nlpzoom/refs/heads/main/sc
 chmod 755 $save_dir/nlpzoom.sh
 touch $save_dir/.env
 
+if ! echo "$PATH" | grep -q "$save_dir"; then
+    echo "export PATH=\"$save_dir:\$PATH\"" >> "$HOME/.zshrc"
+    echo "\"source .zshrc\"を実行してください．"
+fi
+
 echo "✅Install Completed!"
